8576652a7d6963c3d6e05594023dbac2fdd52cae
Jackelyne <jackelyneramosbrito@gmail.com>
2025-09-23 09:48:51 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 # <!-- main.py(logica do codigo e menu) -->
+import sqlite3 
+# Conectar ao banco biblioteca.db.
+import sqlite3
+
+# Conectar ao banco de dados
+conn = sqlite3.connect('biblioteca.db')
+cursor = conn.cursor()
+# • Criar a tabela livros com os campos:
+# Criar a tabela livros
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano INTEGER NOT NULL,
+    disponivel TEXT NOT NULL CHECK(disponivel IN ('Sim', 'Não'))
+)
+''')
+
+# Commit e fechamento da conexão
+conn.commit()
+conn.close()
+
+print("Banco de dados e tabela 'livros' criados com sucesso.")
+
+# o id → chave primária (INTEGER, autoincremento).
+# o título → texto obrigatório.
+# o autor → texto obrigatório.
+# o ano → número inteiro.
+# o disponível → texto (valores "Sim" ou "Não").
